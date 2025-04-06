7b08ef754dcd9cacec92d6c7b9b919050865ecd5
anki079 <42626040+anki079@users.noreply.github.com>
2025-04-05 21:57:07 -0400

    changed port number

--- data_parallel.py
+++ data_parallel.py
@@ -33,7 +33,7 @@ class TextDataset(Dataset):
 
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
+    os.environ['MASTER_PORT'] = '29500'
     
     # initialize the process group
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
