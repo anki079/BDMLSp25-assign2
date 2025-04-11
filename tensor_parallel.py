7e99b3a064c4aba79931f1e417085c104dbbfca8
anki079 <42626040+anki079@users.noreply.github.com>
2025-04-11 03:14:11 -0400

    edit ds config

--- tensor_parallel.py
+++ tensor_parallel.py
@@ -81,9 +81,9 @@ def main():
     model.config.use_cache = False
 
     ds_config = {
-        "train_batch_size": 8, 
+        "train_batch_size": "auto", 
 
-        "gradient_accumulation_steps": 8, 
+        "gradient_accumulation_steps": "auto", 
 
         "bf16": {
             "enabled": True
